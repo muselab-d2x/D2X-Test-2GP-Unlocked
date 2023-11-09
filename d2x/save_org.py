e2ff0f03e68142e0eb01f84c669adb9b9decfeef
Jason Lantz <jason@muselab.com>
2023-11-08 20:08:45 -0600

    Create save_org.py
    [ci skip]

---
+++ d2x/save_org.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+import coreapi
+from cumulusci.core.exceptions import SfdxOrgException
+from cumulusci.core.runtime import BaseCumulusCI
+from cumulusci.core.sfdx import sfdx
+
+org_name = sys.argv[1]
+
+cci = BaseCumulusCI(load_keychain=True)
+org = cci.keychain.get_org("feature")
+results = sfdx("org display --verbose --json", org)
+stderr_list = [line.strip() for line in p.stderr_text]
+stdout_list = [line.strip() for line in p.stdout_text]
+
+if p.returncode:
+    self.logger.error(f"Return code: {p.returncode}")
+    for line in stderr_list:
+        self.logger.error(line)
+    for line in stdout_list:
+        self.logger.error(line)
+    message = f"\nstderr:\n{nl.join(stderr_list)}"
+    message += f"\nstdout:\n{nl.join(stdout_list)}"
+    raise SfdxOrgException(message)
+
+else:
+    try:
+        org_info = json.loads("".join(stdout_list))
+    except Exception as e:
+        raise SfdxOrgException(
+            "Failed to parse json from output.\n  "
+            f"Exception: {e.__class__.__name__}\n  Output: {''.join(stdout_list)}"
+        )
+    auth_url = org_info["result"]["sfdxAuthUrl"]
+  
+org_config = org.config
+org_config["sfdxAuthUrl"] = results["result"]["sfdxAuthUrl"]
+
+auth = coreapi.auth.TokenAuthentication(
+    scheme='Token',
+    token=os.environ.get("D2X_CLOUD_TOKEN"),
+)
+client = coreapi.Client(auth=auth)
+schema = client.get(os.environ.get("D2X_CLOUD_BASE_URL") + "/docs/")
+
+client.action(schema, ['scratch-create-request','complete'], params={
+  "id": os.environ.get("SCRATCH_CREATE_REQUEST_ID"),
+  "org_config": org_config,
+})
